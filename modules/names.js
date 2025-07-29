0238ec9959146140365a5d686e863fdbec081f8f
GlowingPiranha <ikimus01@gmail.com>
2025-07-29 17:27:50 +0200

    first commit

---
+++ modules/names.js
@@ -0,0 +1,11 @@
+// Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+// Esporta la funzione dal file.
+
+const names = (firstName, lastName) => {
+  return {
+    firstName,
+    lastName
+  }
+}
+
+module.exports = names;  
